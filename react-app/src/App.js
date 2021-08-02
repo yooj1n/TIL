500ff3ef2b6eef16d19f1f3c7433db996f154cc6
YOOJIN <ujin07150@gmail.com>
2021-08-02 22:32:05 +0900

    Component,Props,State,Event 개념

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,49 +1,47 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from './components/Subject';
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode : 'welcome',
+      subject : {title : 'WEB', sub : 'World Wide Web!'},
+      welcome : {title:'welcome', desc:'Hello,React!!'},
+      contents : [
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interacitve'},
+      ]
+    }
+  }
   render() {
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject></Subject>
-        <TOC></TOC>
-        <Content></Content>
+    {/*     <Subject 
+          title={this.state.subject.title} 
+          sub ={this.state.subject.sub}>
+        </Subject> */}
+      <header>
+        <h1><a href="/" onClick={function(e){
+          console.log(e);
+          e.preventDefault();
+        }}>{this.state.subject.title}</a></h1>
+        {this.state.subject.sub}
+      </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title = {_title} desc = {_desc}></Content>
       </div>
     );
   }
---
+++ react-app/src/components/TOC.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+  render() {
+    let lists = [];
+    let data = this.props.data;
+    let i = 0;
+    while(i < data.length){
+      lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+      i = i + 1;
+    }
+
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC;

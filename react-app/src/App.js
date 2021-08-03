ccd5a522449259a06672d03b9136127665b3dbc6
YOOJIN <ujin07150@gmail.com>
2021-08-03 22:36:41 +0900

    Event state 변경,bind,setState 함수 이해  / Component evnet 만들기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode : 'welcome',
+      mode : 'read',
+      selected_content_id : 2,
       subject : {title : 'WEB', sub : 'World Wide Web!'},
       welcome : {title:'welcome', desc:'Hello,React!!'},
       contents : [
@@ -24,23 +25,47 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      let i = 0;
+      while( i < this.state.contents.length) {
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc; 
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
-    {/*     <Subject 
+        <Subject 
           title={this.state.subject.title} 
-          sub ={this.state.subject.sub}>
-        </Subject> */}
-      <header>
+          sub ={this.state.subject.sub}
+          onChangePage = {function(){
+            this.setState({mode:'welcome'});
+            }.bind(this)}
+        >
+        </Subject>
+    {/*   <header>
         <h1><a href="/" onClick={function(e){
           console.log(e);
           e.preventDefault();
-        }}>{this.state.subject.title}</a></h1>
+          //this.state.mode = 'welcome'
+          this.setState({
+            mode : 'welcome'
+          });
+        }.bind(this)}>{this.state.subject.title}</a></h1>
         {this.state.subject.sub}
-      </header>
-        <TOC data={this.state.contents}></TOC>
+      </header> */}
+        <TOC 
+        onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id : Number(id)
+          });
+        }.bind(this)} 
+        data={this.state.contents}>
+        </TOC>
         <Content title = {_title} desc = {_desc}></Content>
       </div>
     );

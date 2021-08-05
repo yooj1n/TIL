0d32ae6bbbf90b76c1a7487eaa4da12a9aa4c0a1
YOOJIN <ujin07150@gmail.com>
2021-08-05 22:31:38 +0900

    MovieApp first commit

---
+++ React/movie_app_2021/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading : false})
+    }, 6000);
+  }
+  render() {
+    const {isLoading} = this.state;
+      return (
+        <div>
+      {isLoading ? "Loading..." : "we are ready"}
+      </div>
+      )
+  }
+}
+
+
+
+export default App;

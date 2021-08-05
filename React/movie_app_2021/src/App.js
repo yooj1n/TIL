4ed574a6a65dc281e0e8ef86cabb65c5bbfb8b37
YOOJIN <ujin07150@gmail.com>
2021-08-06 01:37:01 +0900

    axios 사용, data 정리

--- React/movie_app_2021/src/App.js
+++ React/movie_app_2021/src/App.js
@@ -1,25 +1,53 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
+  getMovies = async() => {
+    const {
+      data : {
+        data : {
+          movies }
+        }
+      } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
+    this.setState({movies, isLoading : false});
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading : false})
-    }, 6000);
+    this.getMovies();
+    
   }
   render() {
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
       return (
-        <div>
-      {isLoading ? "Loading..." : "we are ready"}
-      </div>
-      )
+        <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
-
-
 
 export default App;

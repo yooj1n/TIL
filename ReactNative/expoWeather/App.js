9888f77f36af37753ea87ccd4ed5646b4ea5082f
YOOJIN <ujin07150@gmail.com>
2021-08-12 01:57:30 +0900

    titles and subtitle and done

--- ReactNative/expoWeather/App.js
+++ ReactNative/expoWeather/App.js
@@ -46,8 +46,5 @@ export default class extends React.Component {
   render() {
     const { isLoading, temp, condition } = this.state;
     return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
-  
-    
-    
   }
 }

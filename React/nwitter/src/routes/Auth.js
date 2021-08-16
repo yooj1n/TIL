6ca81736dabaa247a559d522c832ab2252b18a6f
YOOJIN <ujin07150@gmail.com>
2021-08-16 17:44:35 +0900

    social login, and logout

--- React/nwitter/src/routes/Auth.js
+++ React/nwitter/src/routes/Auth.js
@@ -1,10 +1,11 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {target: {name, value}} = event;
     if(name === "email") {
@@ -26,19 +27,35 @@ const Auth = () => {
       }
       console.log(data);
     } catch(error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {target:{name}} = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  }
   return (
     <div>
   <form onSubmit={onSubmit}>
     <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
     <input name="password" type="password" placeholder="Paswwsord" required value={password} onChange={onChange}/>
     <input type ="submit" value={newAccount ? "Create Account" : "Log In"} />
+  {error}
   </form>
+  <span onClick={toggleAccount}>
+    {newAccount ? "Sign In" : "Create Account"}
+  </span>
   <div>
-    <button>Continue with Google</button>
-    <button>Continue with Github</button>
+    <button onClick={onSocialClick} name="google">Continue with Google</button>
+    <button onClick={onSocialClick} name="github">Continue with Github</button>
   </div>
 </div>
   );

2fcb41137752373fb089b3c42f303d468b0ac883
YOOJIN <ujin07150@gmail.com>
2021-08-17 01:43:10 +0900

    Form and Database Setup and Getting the Nweets

--- React/nwitter/src/routes/Home.js
+++ React/nwitter/src/routes/Home.js
@@ -1,4 +1,48 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createAt: Date.now()
+    })
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {target : {value}} = event;
+    setNweet(value);
+  }
+return (
+  <div>
+    <form onSubmit={onSubmit}>
+      <input value={nweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
+      <input type="submit" value="Nweet" />
+    </form>
+    <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
+  </div>
+  );
+};
 export default Home;

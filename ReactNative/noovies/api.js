58373df53415b8cec8340a4a870a00b2b8043565
YOOJIN <ujin07150@gmail.com>
2021-08-23 18:58:38 +0900

    API Layers

---
+++ ReactNative/noovies/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const TMDB_KEY = "0a5c0d8acd8423f2bb24153b5937e1c7"
+
+const makeRequest = (path, params) => 
+axios.get(`https://api.themoviedb.org/3${path}`, {
+  params : {
+    //object의 content를 get하는 것을 의미
+    ...params,
+    api_key : TMDB_KEY
+  }
+})
+
+const getAnything = async (path, params = {}) => {
+  try {
+    const {
+      data: { results },
+      data
+    } = await makeRequest(path, params);
+    return [results || data, null];
+  } catch (e) {
+    console.log(e);
+    return [null, e];
+  }
+};
+
+export const movieApi = {
+  //request를 하는 functions
+  nowPlaying: () => getAnything("/movie/now_playing"),
+  popular: () => getAnything("/movie/popular"),
+  upcoming: () => getAnything("/movie/upcoming", { region: "kr" }),
+  search: query => getAnything("/search/movie", { query }),
+  movie: id => getAnything(`/movie/${id}`),
+  discover: () => getAnything("/discover/movie")
+}
+
+export const tvApi = {
+  today: () => getAnything("/tv/airing_today"),
+  thisWeek: () => getAnything("/tv/on_the_air"),
+  topRated: () => getAnything("/tv/top_rated"),
+  popular: () => getAnything("/tv/popular"),
+  search: query => getAnything("/search/tv", { query }),
+  show: id => getAnything(`/tv/${id}`)
+}
--- ReactNative/noovies/screens/Movies.js
+++ ReactNative/noovies/screens/Movies.js
@@ -1,9 +1,36 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { movieApi } from "../api";
 
-export default ({navigation}) => (
-    <View style={{flex:1, backgroundColor: "black"}}>
-    <Text>Movies</Text>
-    <Button title="Movie" onPress={() => navigation.navigate("Detail")} />
+
+export default () => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null
+  });
+  const getData = async () => {
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      popular,
+      upcoming,
+      nowPlayingError,
+      popularError,
+      upcomingError
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
     </View>
   );
+};

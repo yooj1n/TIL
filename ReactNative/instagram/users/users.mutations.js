48cfaead467f92332b21bffbaf0288e828abb9e4
YOOJIN <ujin07150@gmail.com>
2021-09-02 01:49:37 +0900

    bcrypt hash password

--- ReactNative/instagram/users/users.mutations.js
+++ ReactNative/instagram/users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import client from "../client"
 
 export default {
@@ -23,7 +24,17 @@ export default {
         }
       })
       // hash password
+      const uglyPassword = await bcrypt.hash(password, 10);
       // save and return the user
+      return client.user.create({
+        data : {
+          username,
+          email,
+          firstName,
+          lastName,
+          password : uglyPassword,
+        },
+      })
     },
   }
 }

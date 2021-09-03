3363c487bbd01f820df759de7a664e62a2baeac0
YOOJIN <ujin07150@gmail.com>
2021-09-03 20:50:23 +0900

    authentication

--- ReactNative/instagram/users/editProfile/editProfile.resolvers.js
+++ ReactNative/instagram/users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,42 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../../client";
+
 export default {
-  Mutation : {
-    editProfile: () => console.log("hi"),
-  }
-}
+  Mutation: {
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword },
+      { loggedInUser }
+    ) => {
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+      console.log(loggedInUser);
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id: loggedInUser.id,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile.",
+        };
+      }
+    },
+  },
+};

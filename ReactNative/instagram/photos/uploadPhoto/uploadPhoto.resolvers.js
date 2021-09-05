4dbc1858bb5bcc9d146a0d9f59c5f93a1c242a4e
YOOJIN <ujin07150@gmail.com>
2021-09-05 20:19:19 +0900

    upload photo

---
+++ ReactNative/instagram/photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -0,0 +1,34 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+export default {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
+        if (caption) {
+          const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+        }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
+      }
+    ),
+  },
+};

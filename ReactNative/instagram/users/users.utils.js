38e98e3aa6b76685e8144cde0740d4dc007f74eb
YOOJIN <ujin07150@gmail.com>
2021-09-06 21:17:33 +0900

    edit Comment

--- ReactNative/instagram/users/users.utils.js
+++ ReactNative/instagram/users/users.utils.js
@@ -25,10 +25,15 @@ export const protectedResolver = (ourResolver) => (
   info
 ) => {
   if (!context.loggedInUser) {
-    return {
-      ok: false,
-      error: "Please log in to perform this action.",
-    };
+    const query = info.operation.operation === "query";
+    if (query) {
+      return null;
+    } else {
+      return {
+        ok: false,
+        error: "Please log in to perform this action.",
+      };
+    }
   }
   return ourResolver(root, args, context, info);
 };

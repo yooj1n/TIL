c76bbbb03843a36f2df03706c5c445f16a27343d
YOOJIN <ujin07150@gmail.com>
2021-09-06 18:51:57 +0900

    Like Unlike Photos

--- ReactNative/instagram/photos/photos.typeDefs.js
+++ ReactNative/instagram/photos/photos.typeDefs.js
@@ -6,6 +6,7 @@ export default gql`
     user: User!
     file: String!
     caption: String
+    likes: Int!
     hashtags: [Hashtag]
     createdAt: String!
     updatedAt: String!
@@ -15,6 +16,12 @@ export default gql`
     hashtag: String!
     photos(page: Int!): [Photo]
     totalPhotos: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Like {
+    id: Int!
+    photo: Photo!
     createdAt: String!
     updatedAt: String!
   }

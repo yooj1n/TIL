6c5b99f4ac341e4a3fc124280fa7750bba1f2f14
YOOJIN <ujin07150@gmail.com>
2021-09-08 00:42:21 +0900

    Filtering and Authentication subscription

--- ReactNative/instagram/server.js
+++ ReactNative/instagram/server.js
@@ -1,38 +1,79 @@
 require("dotenv").config();
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { typeDefs, resolvers } from "./schema";
-import { getUser, protectResolver } from "./users/users.utils";
-//import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import {graphqlUploadExpress} from "graphql-upload";
 import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { typeDefs, resolvers } from "./schema";
+import { getUser } from "./users/users.utils";
+import { graphqlUploadExpress } from "graphql-upload";
 
 const PORT = process.env.PORT;
-const app = express();
+
 const startServer = async () => {
-  const server = new ApolloServer({
-    resolvers,
+  const app = express();
+  const httpServer = createServer(app);
+  const schema = makeExecutableSchema({
     typeDefs,
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-        protectResolver,
-      };
+    resolvers,
+  });
+
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+      onConnect: async ({ token }, webSocket, context) => {
+        if (!token) {
+          throw new Error("You can't listen.");
+        }
+        console.log("Connected!");
+        return { loggedInUser: await getUser(token) };
+      },
+      onDisconnect(webSocket, context) {
+        console.log("Disconnected!");
+      },
     },
-    //plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    { server: httpServer }
+  ); //subscription 서버를 만듭니다.
+
+  const server = new ApolloServer({
+    schema,
+    context: async (ctx) => {
+      if (ctx.req) {
+        return {
+          loggedInUser: await getUser(ctx.req.headers.token),
+        };
+      }
+    },
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
   });
 
   await server.start();
 
-  app.use(graphqlUploadExpress());
   app.use(logger("tiny"));
-  server.applyMiddleware({app});
+  app.use(graphqlUploadExpress());
+  server.applyMiddleware({ app });
   app.use("/static", express.static("uploads"));
 
-  await new Promise(r => app.listen({port:PORT},r))
-  .then(() =>
-      console.log(`🚀 Server is running on http://localhost:${PORT}${server.graphqlPath} ✅`)
-    );
-}
-
+  httpServer.listen(PORT, () =>
+    console.log(
+      `🚀 Server is running on http://localhost:${PORT}${server.graphqlPath} ✅`
+    )
+  );
+};
 startServer();

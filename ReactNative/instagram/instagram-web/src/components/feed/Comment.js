14630ef22ea427253c9285bd9bb6ad3bbe3b5b1c
YOOJIN <ujin07150@gmail.com>
2021-09-14 20:44:58 +0900

    Parsing Hashtags 수정

--- ReactNative/instagram/instagram-web/src/components/feed/Comment.js
+++ ReactNative/instagram/instagram-web/src/components/feed/Comment.js
@@ -1,12 +1,14 @@
+import React from "react";
 import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
+import { Link } from "react-router-dom";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.accent};
     cursor: pointer;
@@ -17,21 +19,22 @@ const CommentCaption = styled.span`
 `;
 
 function Comment({ author, payload }) {
-  // allowed 된 태그들만 html로 해석될 수 있도록 만들어줌
-  const cleanedPayload = sanitizeHtml(
-    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
-    {
-      allowedTags: ["mark"],
-    }
-  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      {/* 이 prop은 텍스트가 아니라 html로 해석될 수 있도록 만들어줌 */}
-      <CommentCaption dangerouslySetInnerHTML={{
-        __html: cleanedPayload,
-      }}
-      />
+      <CommentCaption>
+        {/* sanitizeHtml과 dangerouslySetInnerHTML를 이용하여 parsing하면 클릭하여 링크로 보내기 힘들다*/}
+        {payload.split(" ").map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            //기본적인 fragment <>에는 key를 줄 수 없어 React에서 제공하는 Fragment를 사용한다.
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }

778cc30ed6a427aee2bb91334451d835657457fb
YOOJIN <ujin07150@gmail.com>
2021-09-19 20:06:03 +0900

    navigation.navigate

--- ReactNative/instagram/instagram-native/screens/LogIn.js
+++ ReactNative/instagram/instagram-native/screens/LogIn.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function LogIn(){
+export default function LogIn({navigation}){
   return (
     <View>
       <Text>LogIn</Text>
+      <TouchableOpacity onPress = {() => navigation.navigate("CreateAccount")}>
+        <View>
+          <Text>Go to Create Account</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }

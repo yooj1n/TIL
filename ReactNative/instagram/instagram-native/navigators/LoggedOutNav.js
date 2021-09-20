1adb33b3f65421d4edf8867ee059abf32760bed2
YOOJIN <ujin07150@gmail.com>
2021-09-20 22:10:05 +0900

    modified screenoptions

--- ReactNative/instagram/instagram-native/navigators/LoggedOutNav.js
+++ ReactNative/instagram/instagram-native/navigators/LoggedOutNav.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from '@react-navigation/stack';
 import Welcome from "../screens/Welcome";
 import CreateAccount from "../screens/CreateAccount";
 import LogIn from "../screens/LogIn";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function LoggedOutNav(){
   return (
-    <Stack.Navigator>
+    <Stack.Navigator 
+    screenOptions={{
+      headerBackTitleVisible:false
+    }}>
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="LogIn" component={LogIn} />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />

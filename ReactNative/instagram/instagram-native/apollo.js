ff96d97ace09f4d045c9be511256fbb223fd2c35
YOOJIN <ujin07150@gmail.com>
2021-09-28 21:21:27 +0900

    asyncstorage modify

--- ReactNative/instagram/instagram-native/apollo.js
+++ ReactNative/instagram/instagram-native/apollo.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
 
 export const logUserIn = async (token) => {
   await AsyncStorage.multiSet([
@@ -9,10 +10,11 @@ export const logUserIn = async (token) => {
     ["loggedIn", JSON.stringify("yes")],
   ]);
   isLoggedInVar(true);
+  tokenVar(token);
 };
 
 const client = new ApolloClient({
-  uri: "https://pink-lizard-50.loca.lt/graphql",
+  uri: "https://splendid-cougar-2.loca.lt/graphql", //localtunnel port 4000
   cache: new InMemoryCache(),
 });
 export default client; 

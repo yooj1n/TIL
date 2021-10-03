34a3463007b2403fbd8f0ba3a74d332b671d45bf
YOOJIN <ujin07150@gmail.com>
2021-10-03 20:42:45 +0900

    scrollIntoView function

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -19,8 +19,16 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null){
     return ;
   }
-
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior:'smooth'});
+  scrollIntoView(link);
 })
+
+//Handle click on "contact me" button on Home
+const homecontactBtn = document.querySelector('.home__contact');
+homecontactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior:'smooth'});
+}

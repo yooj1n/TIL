aa8698ea4ed2a7ff8e3a03ae5c4f24347dd31ecc
YOOJIN <ujin07150@gmail.com>
2021-10-07 19:21:19 +0900

    navbar menu toggle and project UI tweak

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -11,6 +11,7 @@ document.addEventListener('scroll', () => {
   }
 })
 
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -19,7 +20,14 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null){
     return ;
   }
+  navbarMenu.classList.remove('open')
   scrollIntoView(link);
+})
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 })
 
 //Handle click on "contact me" button on Home

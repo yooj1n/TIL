9cf3c1d6daff75c3a7ad8ccf2618c31ea9f18319
YOOJIN <ujin07150@gmail.com>
2021-10-16 22:37:33 +0900

    card dismiss

--- ReactNative/interaction/App.js
+++ ReactNative/interaction/App.js
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Animated, PanResponder, View } from 'react-native';
 import styled from 'styled-components/native';
 import { Ionicons } from "@expo/vector-icons";
+import icons from './icons';
 
 const Container = styled.View`
 flex: 1;
@@ -81,26 +82,33 @@ export default function App() {
     onPanResponderGrant: () => onPressIn.start(),
     onPanResponderRelease: (_, {dx}) => {
       if (dx < -250) {
-        goLeft.start();
+        goLeft.start(onDismiss);
       } else if( dx > 250) {
-        goRight.start();
+        goRight.start(onDismiss);
       } else {
         Animated.parallel([onPressOut,goCenter]).start();
       }
     },
   })
 ).current
+//state
+const [index, setIndex] = useState(0);
+const onDismiss = () => {
+  scale.setValue(1);
+  position.setValue(0);
+  setIndex((prev) => prev + 1)
+}
 const closePress = () => {
-  goLeft.start();
+  goLeft.start(onDismiss);
 }
 const checkPress = () => {
-  goRight.start();
+  goRight.start(onDismiss);
 }
   return (
   <Container>
     <CardContainer>
     <Card style={{transform: [{scale : secondScale}]}}>
-    <Ionicons name="beer" color= "#192a56" size={98}/>
+    <Ionicons name={icons[index + 1]} color= "#192a56" size={98}/>
     </Card>
     <Card
     {...panResponder.panHandlers}
@@ -111,7 +119,7 @@ const checkPress = () => {
           {rotateZ: rotation},
         ],
       }}>
-    <Ionicons name="pizza" color= "#192a56" size={98}/>
+    <Ionicons name={icons[index]} color= "#192a56" size={98}/>
     </Card>
     </CardContainer>
     <BtnContainer>

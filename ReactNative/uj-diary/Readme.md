91810f5e1436d9066a0427c4f889a695b9a97108
YOOJIN <ujin07150@gmail.com>
2021-10-26 14:30:33 +0900

    colors 설정, navigator 설정, Home screen styling

---
+++ ReactNative/uj-diary/Readme.md
@@ -0,0 +1,9 @@
+### setup
+
+### Home Screen
+
+- main colors 미리 지정
+- Navigator.js를 만들고 Tab navigator import -> Tab screen 생성
+- screens 폴더, 파일 생성(Tab screen)
+- App.js\_ NavigatorContainer 안에 Navigator 삽입
+- Home.js styling
---
+++ ReactNative/uj-diary/screens/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../colors";
+
+const View = styled.View`
+flex:1;
+padding: 0px 50px;
+padding-top: 100px;
+background-color: ${colors.bgColor};
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 28px;
+  margin-bottom: 100px;
+
+`;
+const Btn = styled.TouchableOpacity`
+position: absolute;
+bottom: 50px;
+right: 50px;
+width: 80px;
+height: 80px;
+background-color: ${colors.btnColor};
+border-radius: 40px;
+justify-content: center;
+align-items: center;
+box-shadow: 1px 1px 5px rgba(0,0,0,0.3);
+`;
+
+const Home = ({navigation : {navigate}}) => (
+  <View>
+    <Title>My journal</Title>
+    <Btn onPress={() => navigate("Write")}>
+      <Ionicons name="add" color="white" size={40}/>
+    </Btn>
+  </View>
+);
+
+export default Home;

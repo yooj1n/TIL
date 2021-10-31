4a3b991454e64831d8b0978dbcf1d10b818502b7
YOOJIN <ujin07150@gmail.com>
2021-10-31 20:09:32 +0900

    delete feeling and LayoutAnimation.spring

--- ReactNative/uj-diary/screens/Home.js
+++ ReactNative/uj-diary/screens/Home.js
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import colors from "../colors";
 import { useDB } from "../context";
-import { FlatList } from "react-native";
+import { FlatList, LayoutAnimation, TouchableOpacity } from "react-native";
 
 const View = styled.View`
 flex:1;
@@ -56,16 +56,27 @@ const Home = ({navigation : {navigate}}) => {
   const realm = useDB();
   const [feelings, setFeelings] = useState([]);
   useEffect(() => {
+    //컴포넌트가 mount 되었을 때
     const feelings = realm.objects("Feeling");
-    setFeelings(feelings);
-    feelings.addListener(() => {
-      const feelings = realm.objects("Feeling");
-      setFeelings(feelings);
+    //feelings object에 무슨일이 생겼을 때 호출
+    feelings.addListener((feelings, changes) => {
+      //state에 어떤 변화가 생겼든지 animate 하고 싶어
+      LayoutAnimation.spring();
+      setFeelings(feelings.sorted("_id", true))
     });
+    //컴포넌트가 unmount 되었을 때 event listener를 지워지게함
     return () => {
       feelings.removeAllListeners();
     }
   }, [])
+  const onPress = (id) => {
+    //delete, update 등 realm.write안에서 실행해줘야함
+    realm.write(() => {
+      //id에 따라 feeling을 찾고, 삭제
+      const feeling = realm.objectForPrimaryKey("Feeling", id);
+      realm.delete(feeling);
+    })
+  }
   return (
     <View>
       <Title>MY STORY</Title>
@@ -75,10 +86,12 @@ const Home = ({navigation : {navigate}}) => {
        contentContainerStyle={{paddingVertical: 10}}
        keyExtractor={(feeling) => feeling._id + ""} 
        renderItem={({item}) => (
-        <Record>
-          <Emotion>{item.emotion}</Emotion>
-          <Message>{item.message}</Message>
-        </Record>
+        <TouchableOpacity onPress={() => onPress(item._id)}>
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        </TouchableOpacity>
        )}
        />
       <Btn onPress={() => navigate("Write")}>

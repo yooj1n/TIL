191977f53f32235fd3087da9453a55b987d10896
YOOJIN <ujin07150@gmail.com>
2022-01-14 00:55:25 +0900

    object.assign으로 setToDos

--- ReactNative/workhard-travelhard/App.js
+++ ReactNative/workhard-travelhard/App.js
@@ -7,9 +7,22 @@ import { theme } from './color';
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("")
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if(text === "") {
+      return
+    }
+    //object.assign({toDos+합칠object}, toDos, {합칠object})
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: {text, work: working},
+    })
+    setToDos(newToDos);
+    setText("");
+  }
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -23,7 +36,9 @@ export default function App() {
       </View>
       <TextInput
         onChangeText={onChangeText}
+        onSubmitEditing={addToDo}
         value={text}
+        returnKeyType="done"
         placeholder={working ? "Add a To do" : "Where do you want to go?"}
         style={styles.input} />
     </View>

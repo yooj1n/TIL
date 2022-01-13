fd27f04d3f5a52e5ed55773ddb18f4a5ff0f07f1
YOOJIN <ujin07150@gmail.com>
2022-01-14 00:25:56 +0900

    Section and TextInput

---
+++ ReactNative/workhard-travelhard/App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { theme } from './color';
+
+export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("")
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working ? "Add a To do" : "Where do you want to go?"}
+        style={styles.input} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 36,
+    fontWeight: "600",
+    color: theme.grey,
+  },
+  input : {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal:15,
+    borderRadius:20,
+    marginTop:15,
+  }
+});

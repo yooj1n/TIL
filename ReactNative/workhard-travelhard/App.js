ecfc91594129778763dd0c50df73949c1c459874
YOOJIN <ujin07150@gmail.com>
2022-01-20 01:48:09 +0900

    delete To Dos

--- ReactNative/workhard-travelhard/App.js
+++ ReactNative/workhard-travelhard/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { Fontisto } from "@expo/vector-icons";
 import { theme } from './color';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -44,6 +45,19 @@ export default function App() {
     await saveToDos(newToDos)
     setText("");
   }
+  const deleteTodo = (key) => {
+    Alert.alert("Delete To Do","Are you sure", [
+      {text:"Cancel"},
+      {text:"I'm sure", 
+        onPress: async() => {
+          const newToDos = {...toDos}
+          delete newToDos[key]
+          setToDos(newToDos);
+          await saveToDos(newToDos)
+      }}
+    ])
+
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -67,6 +81,9 @@ export default function App() {
             toDos[key].working === working ? (
               <View style={styles.toDo} key={key}>
                 <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                <TouchableOpacity onPress={() => deleteTodo(key)}>
+                  <Fontisto name="trash" size={14} color="white" />
+                </TouchableOpacity>
               </View>
             ) : null
           )}
@@ -105,6 +122,9 @@ const styles = StyleSheet.create({
     paddingVertical:15,
     marginBottom:10,
     borderRadius:10,
+    flexDirection: "row",
+    alignItems:"center",
+    justifyContent:"space-between"
   },
   toDoText : {
     color: "white",

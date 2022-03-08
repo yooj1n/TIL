a097f8b25c5dc857df1def9b348bf37dc33ac8b1
YOOJIN <ujin07150@gmail.com>
2022-03-09 01:32:40 +0900

    paint to dos

--- Redux/vanilla-redux/src/index.js
+++ Redux/vanilla-redux/src/index.js
@@ -7,6 +7,21 @@ const ul = document.querySelector("ul");
 const ADD = "ADD";
 const DELETE = "DELETE";
 
+const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+    id: Date.now(),
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
@@ -23,11 +38,37 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDelToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDo = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DELETE";
+    btn.addEventListener("click", dispatchDelToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDo);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD, text: toDo, id: Date.now() });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);

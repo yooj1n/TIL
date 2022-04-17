744bfd41abd7b6ec2d8800e54c55a6f27f1925d3
YOOJIN <ujin07150@gmail.com>
2022-04-17 17:38:44 +0900

    Feed modify

--- ReactNative/myLog/components/FeedListItem.js
+++ ReactNative/myLog/components/FeedListItem.js
@@ -1,3 +1,6 @@
+import {useNavigation} from '@react-navigation/native';
+import {format, formatDistanceToNow} from 'date-fns';
+import {ko} from 'date-fns/locale';
 import React from 'react';
 import {Platform, Pressable, StyleSheet, Text} from 'react-native';
 
@@ -9,15 +12,34 @@ function turncate(text) {
   return replaced.slice(0, 100).concat('...');
 }
 
+function formatDate(date) {
+  const d = new Date(date);
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000;
+
+  if (diff < 60 * 1) {
+    return '방금 전';
+  }
+  if (diff < 60 * 60 * 24 * 3) {
+    return formatDistanceToNow(d, {addSuffix: true, locale: ko});
+  }
+  return format(d, 'PPP EEE p', {locale: ko});
+}
+
 function FeedListItem({log}) {
   const {title, body, date} = log;
+  const navigation = useNavigation();
+  const onPress = () => {
+    navigation.navigate('Write', {log});
+  };
   return (
     <Pressable
       style={({pressed}) => [
         styles.block,
         Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
-      ]}>
-      <Text style={styles.date}>{new Date(date).toLocaleString()}</Text>
+      ]}
+      onPress={onPress}>
+      <Text style={styles.date}>{formatDate(date)}</Text>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.body}>{turncate(body)}</Text>
     </Pressable>
--- ReactNative/myLog/screens/WriteScreen.js
+++ ReactNative/myLog/screens/WriteScreen.js
@@ -11,17 +11,28 @@ import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
 import LogContext from '../contexts/LogContext';
 
-function WriteScreen() {
+function WriteScreen({route}) {
+  console.log(JSON.stringify(route, null, 2));
+  const log = route.params?.log;
   const navigation = useNavigation();
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const {onCreate} = useContext(LogContext);
+  const [title, setTitle] = useState(log?.title ?? '');
+  const [body, setBody] = useState(log?.body ?? '');
+  const {onCreate, onModify} = useContext(LogContext);
   const onSave = () => {
-    onCreate({
-      title,
-      body,
-      date: new Date().toISOString(),
-    });
+    if (log) {
+      onModify({
+        id: log.id,
+        date: log.date,
+        title,
+        body,
+      });
+    } else {
+      onCreate({
+        title,
+        body,
+        date: new Date().toISOString(),
+      });
+    }
     navigation.pop();
   };
 

895107127cc067beb61db38b960188a01c80c5d4
YOOJIN <ujin07150@gmail.com>
2022-04-19 01:49:34 +0900

    asyncStorage

--- ReactNative/myLog/contexts/LogContext.js
+++ ReactNative/myLog/contexts/LogContext.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {createContext, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storages/AsyncStorage';
 
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
+  const initialLogsRef = useRef(null);
   const [logs, setLogs] = useState([]);
   const onCreate = ({title, body, date}) => {
     const log = {
@@ -23,6 +25,21 @@ export function LogContextProvider({children}) {
     const nextLogs = logs.filter(log => log.id !== id);
     setLogs(nextLogs);
   };
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+    logsStorage.set(logs);
+  }, [logs]);
   return (
     <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
       {children}
